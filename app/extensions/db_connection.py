228fae32ec71132e57f6004deb27719d14fbecc3
tales rodrigues <tales@DESKTOP-7UT3F5F.lead.fortaleza>
2025-01-28 15:08:56 -0300

    updating db_connection

--- app/extensions/db_connection.py
+++ app/extensions/db_connection.py
@@ -2,8 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 def start_postgres_connection(app):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345@localhost:5432/postgres"
+    
+    #sem necessidade de realizar alterações para execução docker ou no ambiente python local.
+    if(environ.get('DB_URL')):
+        app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345@localhost:5432/postgres"
+   
     db_connection = SQLAlchemy(app)
     
     return db_connection
